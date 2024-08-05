21fad35401cf532e86ff9c8061e40314a9f60833
unknown <wujingbo12l@outlook.com>
2024-08-05 13:30:42 +0800

    Site updated: 2024-08-05 13:30:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/03/告别云担忧：手把手教你本地部署AI大模型/"];function toRandomPost(){
+var posts=["2024/07/23/告别云担忧：手把手教你本地部署AI大模型/","2024/07/20/勇气：雷军与小米的非凡旅程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
